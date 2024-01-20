96193f5e070472d3f22944ef702421b8d799b6fb
Henry Andrus <henry@tinypineapple.com>
2024-01-20 12:07:40 -0700

    Implemented King calculator, tests pass

--- shared/src/main/java/chess/moveCalculators/KingMove.java
+++ shared/src/main/java/chess/moveCalculators/KingMove.java
@@ -16,6 +16,30 @@ public class KingMove extends MoveCalculator {
     }
 
     public HashSet<ChessMove> generateMoves() {
-        return new HashSet<>();
+        int row = this.position.getRow();
+        int col = this.position.getColumn();
+
+        for (int i = -1; i <= 1; i++) {
+            for (int j = -1; j <= 1; j++) {
+                if (i == 0 && j == 0) {
+                    continue;
+                }
+                if ((row + i) >= 8 || (row + i) <= 1) {
+                    continue;
+                }
+                if ((col + j) >= 8 || (col +j) <= 1) {
+                    continue;
+                }
+                ChessPosition potentialLanding = new ChessPosition(row+i, col+j);
+                if (board.getPiece(potentialLanding) == null) {
+                    moves.add(new ChessMove(position, potentialLanding));
+                } else {
+                    if (board.getPiece(potentialLanding).getTeamColor() != pieceColor) {
+                        moves.add(new ChessMove(position, potentialLanding));
+                    }
+                }
+            }
+        }
+        return moves;
     }
 }
